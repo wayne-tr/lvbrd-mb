508ee5af13eb1f70c3c399f81ab22701f8c85ead
Wayne <wayne.tr.1711@gmail.com>
2025-02-13 10:18:54 +0700

    Add confirm page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,6 +4,8 @@ import { Platform } from "react-native";
 
 import { HapticTab } from "@/components/HapticTab";
 import { IconSymbol } from "@/components/ui/IconSymbol";
+import { MaterialIcons } from "@expo/vector-icons";
+
 import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
@@ -42,6 +44,15 @@ export default function TabLayout() {
           title: "Signin",
           tabBarIcon: ({ color }) => (
             <IconSymbol size={28} name="paperplane.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="confirm"
+        options={{
+          title: "Confirm",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="check-circle" size={28} color={color} />
           ),
         }}
       />
